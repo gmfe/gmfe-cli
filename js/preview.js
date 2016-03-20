24c89f3562511aeba52604a2a78b76c5b3f3bfea
liyatang <liyatang@xiaonongnv.com>
2016-03-20 20:20:43 +0800

    c

--- js/preview.js
+++ js/preview.js
@@ -5,10 +5,10 @@ function preview() {
     var diff = sh.exec('git diff', {silent: true});
     if (diff.code === 0) {
         if (diff.stdout !== '') {
-            Log.warn('dirty work directory, please confirm not forgetting git commit', 'adfadf');
+            Log.warn('dirty work directory, please confirm not forgetting git commit');
             return 1;
         }else{
-            Log('gif diff ok');
+            Log.log('gif diff ok');
             return 0;
         }
     } else {
