02d99d41016277583bc26ab30f9cc6bc86b058ac
liyatang <liyatang@xiaonongnv.com>
2016-03-24 12:00:37 +0800

    c

--- js/index.js
+++ js/index.js
@@ -38,7 +38,6 @@ sh.cd(projectPath);
 // sh.exec('pwd');
 
 
-
 // if (argv._.length === 0) {
 //     sh.exec('./bin/gmfe.sh -h');
 //     process.exit(0);
@@ -64,19 +63,49 @@ sh.cd(projectPath);
 const hosts = Util.getOnlineHosts();
 console.log(hosts);
 
-const ssh = new SSH({
-    host: 'guanmai.cn',
-    user: 'liyatang'
-});
-ssh.exec('pwd', {
-    out: (stdout) => {
-        console.log(stdout);
-    },
-    err: (stderr) => {
-        console.log(stderr);
-    }
-}).start();
-
-// sh.exec('ssh -oConnectTimeout=3 -oStrictHostKeyChecking=no liyatang@guanmai.cn', (code, stdout, stderr) => {
-//     console.log(code);
+// const ssh = new SSH({
+//     host: 'guanmai.cn',
+//     user: 'liyatang'
 // });
+// ssh.exec('echo $PATH', {
+//     out(stdout) {
+//         console.log('out');
+//         console.log(stdout);
+//     },
+//     err(stderr){
+//         console.log(stderr);
+//     },
+//     exit(code){
+//         console.log(code)
+//     }
+// }).start();
+
+// // sh.exec('ssh -tt -oConnectTimeout=3 -oStrictHostKeyChecking=no liyatang@guanmai.cn; pwd; exit;', (code, stdout, stderr) => {
+// //     console.log(code, stdout, stderr);
+// });
+
+var Client = require('ssh2').Client;
+
+var conn = new Client();
+conn.on('ready', function () {
+    console.log('Client :: ready');
+    conn.shell(function (err, stream) {
+        if (err) throw err;
+        stream.on('close', function () {
+            console.log('Stream :: close');
+            conn.end();
+        }).on('data', function (data) {
+            console.log('STDOUT: ' + data);
+        }).stderr.on('data', function (data) {
+            console.log('STDERR: ' + data);
+        });
+        stream.end('ls -l\nexit\n');
+    });
+}).connect({
+    host: 'guanmai.cn',
+    port: 22,
+    username: 'liyatang',
+    // password: 'lyt2015',
+    // privateKey: require('fs').readFileSync('/Users/liyatang/.ssh/id_rsa')
+    privateKey: require('fs').readFileSync('/Users/liyatang/gm/git/gmfe/js/id_rsa')
+});
