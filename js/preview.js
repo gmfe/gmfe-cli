b7e15ee69a576c84d2c2bca094382ce2c0f0fa5e
liyatang <liyatang@xiaonongnv.com>
2016-03-27 18:18:44 +0800

    c

--- js/preview.js
+++ js/preview.js
@@ -2,8 +2,8 @@ var sh = require('shelljs');
 var Log = require('./util').Log;
 
 function preview() {
-
     Log.info('Step1: preview');
+
     var diff = sh.exec('git diff', {silent: true});
     if (diff.stdout !== '') {
         Log.warn('Dirty！确保你本地代码是干净的。');
@@ -11,19 +11,15 @@ function preview() {
     }
 
     var branch = sh.exec('git branch', {silent: true});
-    if (branch.stdout !== '* master\n') {
+    if (branch.stdout.indexOf('* master\n') === -1) {
         Log.warn('No master！确保你处于master分支');
         return false;
     }
 
+    // 拉最新代码
     sh.exec('git fetch; git reset --hard origin/master');
 
-    // var mDiff = sh.exec('git diff master origin/master', {silent: true});
-    // if (mDiff.stdout !== '') {
-    //     Log.warn('Your master branch is different from origin/master. Maybe you forgot git push!');
-    //     return false;
-    // }
-
+    // 推送到deploy/preview。预发布环境，目前没有什么用。
     sh.exec('git push origin HEAD:deploy/preview');
 }
 
