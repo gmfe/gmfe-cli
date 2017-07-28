0b9152b4a11eb349a546c2245507e84e36cc1170
zhangsiqi <276987359@qq.com>
2017-07-28 18:36:29 +0800

    发布路径更改

--- js/publish/rollback.js
+++ js/publish/rollback.js
@@ -1,6 +1,8 @@
 const sh = require('shelljs');
 const Log = require('./util').Log;
 const fs = require('fs');
+const path = require('path');
+const Util = require('./util');
 
 function rollback(tag) {
     Log.info('>>>>>>>>>> 执行rollback');
@@ -16,8 +18,10 @@ function rollback(tag) {
     Log.step(`解压 ${fileName}`);
     sh.exec(`tar zxvf ${fileName} -C ./`);
 
-    Log.step('执行后置上线脚本 ./deploy/after_online');
-    sh.exec('./deploy/after_online');
+    Log.step('执行上线脚本');
+
+    const projectName = Util.getProjectPath().split('/').pop().replace('gm_static_', '');
+    sh.exec(`rsync -aztH --exclude .git --exclude .svn --exclude .swp --exclude node_modules --rsh=ssh ./build/ static.cluster.gm:/data/www/static_resource/${projectName}/`);
 }
 
 module.exports = rollback;
