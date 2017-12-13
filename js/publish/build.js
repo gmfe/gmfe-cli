00362830050af7df6a75931a3641d5a9157a5c6f
siqi <zsq276987359@gmail.com>
2017-12-13 16:13:50 +0800

    log fix

--- js/publish/build.js
+++ js/publish/build.js
@@ -5,8 +5,7 @@ function build(branch = 'master') {
     logger.info('>>>>>>>>>> 执行打包');
 
     logger.info('npm run deploy');
-    const deploy = sh.exec(`BRANCH=${branch} npm run deploy`);
-    logger.info(deploy.stdout);
+    sh.exec(`BRANCH=${branch} npm run deploy`);
 
     logger.info('打包完成!');
 }
