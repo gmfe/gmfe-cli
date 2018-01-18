e73300474b88136586ea5318a81c90c693ddfdfa
maltose <liyatang@xiaonongnv.com>
2018-01-18 17:15:40 +0800

    欢迎 一些提示

--- js/test/index.js
+++ js/test/index.js
@@ -2,8 +2,6 @@ const sh = require("shelljs");
 const preview = require('./preview');
 const testCheck = require('./test_check');
 const {getProjectPath, getProjectName, logger, getLastCommit} = require('../util');
-const moment = require('moment');
-const http = require('http');
 
 function init(branch = "master") {
     // 前往工程的父目录
@@ -26,8 +24,6 @@ function init(branch = "master") {
 
     logger.info('>>>>>>>>>> 执行打包');
 
-    dingtalk('开始测试部署', branch);
-
     sh.exec(`BRANCH=${branch} COMMIT=${getLastCommit()} npm run testing`);
 
     logger.info('打包完成!');
@@ -44,17 +40,10 @@ function init(branch = "master") {
 
     logger.info('测试部署完成!');
 
-    dingtalk('接受测试部署', branch);
-
     // event
     process.on('exit', function () {
         logger.info('gmfe exit');
     });
-}
-
-function dingtalk(msg, branch) {
-    const projectName = getProjectName();
-    http.get(`http://test.guanmai.cn:8083/testing/${msg} ${projectName} ${branch} (${moment().format('YYYY-MM-DD HH:mm:ss')})`);
 }
 
 module.exports = init;
